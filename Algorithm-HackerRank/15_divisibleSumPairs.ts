0c6c441f55f4e21a26976b08a3564b6fc06ea5df
kamrunnaharruma <knahar12bd@gmail.com>
2021-10-02 14:02:25 +0600

    https://www.hackerrank.com/challenges/divisible-sum-pairs/problem

--- Algorithm-HackerRank/15_divisibleSumPairs.ts
+++ Algorithm-HackerRank/15_divisibleSumPairs.ts
@@ -1,17 +1,14 @@
 function divisibleSumPairs(n: number, k: number, ar: number[]): number {
     let koyta:number = 0;
     ar.forEach((num, index)=>{
-        ar.forEach((number, ind)=>{
-            if(number<num && (number+num)%k===0){
-                koyta +=1;
-            }
+        if(index !== ar.length-1){
+            ar.slice(index+1, ar.length).forEach((number, ind)=>{
+                if((number+num)%k===0){
+                    koyta +=1;
+                }
 
-        })
-
+            })
+        }
     })
-
-    console.log(koyta)
     return (koyta)
-
 }
-//https://www.hackerrank.com/challenges/divisible-sum-pairs/problem
